e0632547a834d10e9e8b5dd7fa57a99ee58f65d2
kojbakov <kojbakov@gmail.com>
2018-10-05 18:01:11 +0300

    Delete button

--- blog/views.py
+++ blog/views.py
@@ -39,5 +39,12 @@ def post_edit(request, pk):
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
-    return render(request, 'blog/post_edit.html', {'form': form})    
+    return render(request, 'blog/post_edit.html', {'form': form})  
+      
+def post_delete(request, pk):
+    post = get_object_or_404(Post, pk=pk)  
+    post.delete()
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    
+    return render(request, 'blog/post_list.html', {'posts' : posts})
 
