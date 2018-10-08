035bd941eb2a40e53d6a7d38b9b3f34cf54a3042
kojbakov@gmail.com <kojbakov@gmail.com>
2018-10-08 07:42:35 +0300

    Delete button

--- blog/views.py
+++ blog/views.py
@@ -45,6 +45,6 @@ def post_delete(request, pk):
     post = get_object_or_404(Post, pk=pk)  
     post.delete()
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    
-    return render(request, 'blog/post_list.html', {'posts' : posts})
+    return redirect('post_list')
+    #return render(request, 'blog/post_list.html', {'posts' : posts})
 
